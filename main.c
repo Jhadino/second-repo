8cdaebef763b322ef98bf40f6ae1dc7ab67978a4
Курчаков Даниил <mintor18@mail.ru>
2025-09-08 14:54:52 +0300

    изменил

--- main.c
+++ main.c
@@ -4,21 +4,21 @@
 int main() {
     int numbers[] = {2, 8, 15, 27, 64, 10, 125};
     int size = sizeof(numbers) / sizeof(numbers[0]);
-    int k = 3;
+    int k = 2;
     int k_cube = k * k * k;
-    
-    printf("Поиск чисел, кратных %d^3 (%d)...\n", k, k_cube);
-    
+
+    printf("search for numbers multiple of %d^3 (%d)...\n", k, k_cube);
+
     for (int i = 0; i < size; i++) {
         if (numbers[i] % k_cube == 0) {
-            printf("Найдено: %d\n", numbers[i]);
-            printf("Программа завершена.\n");
-            return 0; 
+            printf("find: %d\n", numbers[i]);
+            printf("end.\n");
+            return 0;
         }
     }
- 
-    printf("Чисел, кратных %d^3, не найдено.\n", k);
-    printf("Программа завершена.\n");
-    
+
+    printf(" No.\n", k);
+    printf("END.\n");
+
     return 0;
 }
