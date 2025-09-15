89e7f168f47e58d32a1dbaaebce4f92a810c9f80
Курчаков Даниил <mintor18@mail.ru>
2025-09-15 13:54:35 +0300

    ЖОСКА СДЕЛАЛ

--- main.c
+++ main.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int find_mul(int numbers[], int size, int k) {
+    int k_cube = k * k * k;
+    printf("search numbers %d^3 (%d)...\n", k, k_cube);
+    for (int i = 0; i < size; i++) {
+        if (numbers[i] % k_cube == 0) {
+            printf("Find: %d\n", numbers[i]);
+            return 1;
+        }
+    }
+    printf("No.\n", k);
+    return 0;
+}
+
 int main() {
     int numbers[] = {2, 8, 15, 27, 64, 10, 125};
     int size = sizeof(numbers) / sizeof(numbers[0]);
-    int k = 2;
-    int k_cube = k * k * k;
+    int k = 10;
 
-    printf("search for numbers multiple of %d^3 (%d)...\n", k, k_cube);
 
-    for (int i = 0; i < size; i++) {
-        if (numbers[i] % k_cube == 0) {
-            printf("find: %d\n", numbers[i]);
-            printf("end.\n");
-            return 0;
-        }
-    }
+    int found = find_mul(numbers, size, k);
 
-    printf(" No.\n", k);
     printf("END.\n");
-
-    return 0;
+    return found;
 }
